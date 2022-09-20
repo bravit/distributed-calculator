0b456b42ae944d33b60d731818425f52965a7a9c
Vitaly Bragilevsky <bravit111@gmail.com>
2022-09-20 16:17:37 +0300

    Add Kotlin Divider

---
+++ kotlin/app/src/main/kotlin/divideapp/App.kt
@@ -0,0 +1,30 @@
+package com.example.divideapp
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.request.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello World!"
+        }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 4000) {
+        routing {
+            post("/divide") {
+                val request = Json.decodeFromString<Operands>(call.receiveText())
+                println("Dividing ${request.operandOne} by ${request.operandTwo} (Kotlin)")
+                val result = request.operandOne / request.operandTwo
+                call.respondText("$result")
+            }
+        }
+    }.start(wait = true)
+}
---
+++ kotlin/app/src/main/kotlin/divideapp/Operands.kt
@@ -0,0 +1,5 @@
+package com.example.divideapp
+import kotlinx.serialization.*
+
+@Serializable
+data class Operands (val operandOne: Float, val operandTwo: Float)
