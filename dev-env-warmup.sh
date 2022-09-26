60b23a7f9a4ad1263581f6e607709f3c23efedec
Vitaly Bragilevsky <bravit111@gmail.com>
2022-09-26 10:20:02 +0300

    Add warm-up steps

--- dev-env-warmup.sh
+++ dev-env-warmup.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-cd $HOME
-dapr init
+cd python
+pip3 install wheel python-dotenv flask_cors flask
+cd ../go
+go build .
+cd ../csharp
+export ASPNETCORE_URLS="http://localhost:7001"
+dotnet build
+cd ../node
+npm install
+cd ../react-calculator
+npm install
+npm run buildclient
+cd ../java
+./gradlew build
+cd ../kotlin
+./gradlew build
+cd ../rust
+cargo build
