9b16ed84b603d8d01369199bc7abb0ee4950b395
Vitaly Bragilevsky <bravit111@gmail.com>
2022-10-06 15:22:04 +0300

    Add PHP Subtracter

---
+++ php/src/Controller/SubtractController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
+
+class SubtractController extends AbstractController
+{
+    #[Route('/subtract', name: 'subtract')]
+    public function subtract(Request $request, LoggerInterface $logger): Response
+    {
+        $operands = json_decode($request->getContent(), true);
+        $logger->info("Subtracting ${operands['operandTwo']} from ${operands['operandOne']} (PHP)");
+        return new Response($operands['operandOne'] - $operands['operandTwo']);
+    }
+}
