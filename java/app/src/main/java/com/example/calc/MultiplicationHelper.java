c6424271fb448d4f220e4bdb0d1e5c0ba3db92ad
Vitaly Bragilevsky <vitaly.bragilevsky@jetbrains.com>
2024-02-26 10:57:39 +0200

    Fix java naming

--- java/app/src/main/java/com/example/calc/MultiplicationHelper.java
+++ java/app/src/main/java/com/example/calc/MultiplicationHelper.java
@@ -4,12 +4,12 @@ import spark.Request;
 import spark.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-class AdditionHelper implements spark.Route {
+class MultiplicationHelper implements spark.Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         Operands ops = mapper.readValue(request.body(), Operands.class);
-        System.out.printf("Calculating %2.1f + %2.1f%n", ops.operandOne, ops.operandTwo);
-        return Float.toString(ops.operandOne + ops.operandTwo);
+        System.out.printf("Calculating %2.1f * %2.1f%n", ops.operandOne, ops.operandTwo);
+        return Float.toString(ops.operandOne * ops.operandTwo);
     }
 }
